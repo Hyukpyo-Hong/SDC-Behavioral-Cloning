a4864e62d62b9425e218abd035a2ad475f3878e9
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-02-12 14:52:44 -0500

    my data

--- drive.py
+++ drive.py
@@ -1,86 +1,72 @@
 import argparse
 import base64
+import json
 from datetime import datetime
 import os
 import shutil
-
 import numpy as np
 import socketio
 import eventlet
 import eventlet.wsgi
+import time
 from PIL import Image
-from flask import Flask
+from PIL import ImageOps
+from flask import Flask, render_template
 from io import BytesIO
 
-from keras.models import load_model
+from keras.models import model_from_json
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+
 import scipy.misc as sp
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
+img_gen = None
 shape = (100, 200)
 prev_angle = 0
 
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    if data:
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = float(data["speed"])
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image).astype(np.float32)
-        image_array = sp.imresize(image_array, size=shape)
-        image_array = (image_array[30:96, :]) / 255. - .5
-        steering_angle = float(model.predict(
-            image_array[None, :, :, :], batch_size=1))
-#         if (abs(steering_angle) < 0.1):
-#             throttle = 0.2
-#         else:
-#             if (abs(steering_angle) < 0.15):
-#             	throttle = 0.05  # * 1./(steering_angle if steering_angle>0.1)
-#             else:
-#             	throttle = 0.01  # * 1./(steering_angle if steering_angle>0.1)
+    # The current steering angle of the car
+    steering_angle = data["steering_angle"]
+    # The current throttle of the car
+    throttle = data["throttle"]
+    # The current speed of the car
+    speed = float(data["speed"])
 
-#         print("Angle: ", format(steering_angle, '.3f'),
-#               " Throttle: ", throttle, " Speed: ", speed)
+    # The current image from the center camera of the car
+    imgString = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    image_array = np.asarray(image)
+    image_array = sp.imresize(image_array, size=shape)
+    image_array = image_array[30:96, :]
+    transformed_image_array = image_array[None, :, :, :]
 
-        global prev_angle
-        _diff = steering_angle - prev_angle
-        diff = abs(_diff)
-        if(speed < 6):
-            throttle = 0.6  # Boost when the car start with large degree
-        elif(diff > 0.05):
-            print("Angle stablizing. Original Angle: ",
-                  format(steering_angle, '.3f'))
-            steering_angle = prev_angle + _diff * 0.7
-            throttle = -0.3
-        else:
-            if (abs(steering_angle) < 0.1):
-                throttle = 0.5
-            elif (abs(steering_angle) < 0.15):
-                throttle = 0.2
-            elif (abs(steering_angle) < 0.2):
-                throttle = 0.1
-            elif (abs(steering_angle) < 0.25):
-                throttle = 0.05
-            else:
-                throttle = 0.01  # 3.75<
-        prev_angle = steering_angle
-        send_control(steering_angle, throttle)
-
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
+    #steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    steering_angle = float(model.predict_generator(img_gen.flow(
+        transformed_image_array, batch_size=1), val_samples=1))
+    # The driving model currently just outputs a constant throttle. Feel free
+    # to edit this.
+    if (abs(steering_angle) < 0.1):  # 2.5degree
+        throttle = 0.25
     else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+        if(speed < 7):
+            throttle = 0.5
+        elif (abs(steering_angle) < 0.15):  # 3.75degree
+            throttle = 0.3
+        else:
+            throttle = 0.05
+    print("Angle: ", format(steering_angle * 25, '.2f'), "(",
+          format(steering_angle, '.3f'), ")", " Throttle: ", throttle, " Speed: ", speed)
+    send_control(steering_angle, throttle)
+    # save frame
+    if args.image_folder != '':
+        timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+        image_filename = os.path.join(args.image_folder, timestamp)
+        image.save('{}.jpg'.format(image_filename))
+
 
 
 @sio.on('connect')
@@ -90,22 +76,16 @@ def connect(sid, environ):
 
 
 def send_control(steering_angle, throttle):
-    sio.emit(
-        "steer",
-        data={
-            'steering_angle': steering_angle.__str__(),
-            'throttle': throttle.__str__()
-        },
-        skip_sid=True)
+    sio.emit("steer", data={
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    }, skip_sid=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument(
-        'model',
-        type=str,
-        help='Path to model h5 file. Model should be on the same path.'
-    )
+    parser.add_argument('model', type=str,
+                        help='Path to model definition json. Model weights should be on the same path.')
     parser.add_argument(
         'image_folder',
         type=str,
@@ -113,9 +93,15 @@ if __name__ == '__main__':
         default='',
         help='Path to image folder. This is where the images from the run will be saved.'
     )
-    args = parser.parse_args()
 
-    model = load_model(args.model)
+    args = parser.parse_args()
+    with open(args.model, 'r') as jfile:
+        model = model_from_json(json.load(jfile))
+
+    model.compile("adam", "mse")
+    weights_file = args.model.replace('json', 'h5')
+    model.load_weights(weights_file)
+    img_gen = ImageDataGenerator()
 
     if args.image_folder != '':
         print("Creating image folder at {}".format(args.image_folder))
@@ -128,32 +114,9 @@ if __name__ == '__main__':
     else:
         print("NOT RECORDING THIS RUN ...")
 
+
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
-
-'''
-global prev_angle
-        _diff = steering_angle - prev_angle
-        diff = abs(_diff)
-        if(speed < 15):
-            throttle = 0.6  # Boost when the car start with large degree                   
-        elif(diff>0.05):        	
-        	print("Angle stablizing. Original Angle: ",format(steering_angle, '.3f'))
-        	steering_angle = prev_angle + _diff*0.7
-        	throttle = -0.3        	
-        else:
-            if (abs(steering_angle) < 0.1):
-            	throttle = 0.5
-            elif (abs(steering_angle) < 0.15): 
-            	throttle = 0.2
-            elif (abs(steering_angle) < 0.2): 
-                throttle = 0.1
-            elif (abs(steering_angle) < 0.25): 
-                throttle = 0.05
-            else:
-                throttle = 0.01  # 3.75<
-        prev_angle = steering_angle
-'''
--- model.py
+++ model.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 # Train Parameter
-epoch = 5
-shift = 0.3  # Additional value to if there're images from left and right camera
+epoch = 10
+shift = 0.25  # Additional value to if there're images from left and right camera
 shape = (100, 200)  # Shape of resize before crop
 validate_portion = 0.05
 learning_rate = 0.001
-data_create_or_load = 0  # 0: Create new Dataset/save 1: Load previous Dataset
+data_create_or_load = 1  # 0: Create new Dataset/save 1: Load previous Dataset
 
 # Model
 from keras.models import Sequential
@@ -23,134 +23,131 @@ from keras.optimizers import Adam
 
 
 def model():
-    model = Sequential()
-    elu = ELU(alpha=1.0)
-    model.add(Convolution2D(24, 5, 5, input_shape=(
-        66, 200, 3), subsample=(2, 2), name='C1'))
-    model.add(elu)
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), name='C2'))
-    model.add(elu)
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), name='C3'))
-    model.add(elu)
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(64, 3, 3, name='C4'))
-    model.add(elu)
-    model.add(Convolution2D(64, 3, 3, name='C5'))
-    model.add(elu)
-    model.add(Flatten())
-    model.add(Dense(100, name='L1'))
-    model.add(elu)
-    model.add(Dense(50, name='L2'))
-    model.add(elu)
-    model.add(Dense(10, name='L3'))
-    model.add(Dense(1, name='L4'))
-    return model
+	model = Sequential()
+	model.add(Convolution2D(24, 5, 5, input_shape=(
+		66, 200, 3), subsample=(2, 2), name='C1'))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Convolution2D(36, 5, 5, subsample=(2, 2), name='C2'))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Convolution2D(48, 5, 5, subsample=(2, 2), name='C3'))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Convolution2D(64, 3, 3, name='C4'))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(64, 3, 3, name='C5'))
+	model.add(Activation('relu'))
+	model.add(Flatten())
+	model.add(Dense(100, name='L1'))
+	model.add(Activation('relu'))
+	model.add(Dense(50, name='L2'))
+	model.add(Activation('relu'))
+	model.add(Dense(10, name='L3'))
+	model.add(Dense(1, name='L4'))
+	return model
 
 # Functions
 from scipy.misc import imread
 import scipy.misc as sp
 
-
 def flip_merge(data):
-    length = len(data['imgc'])
-    X_train = []
-    y_train = []
-    count = 0
-    for i in range(3):
-        if data[0][i] == 'center':
-            for i, loc in zip(range(length), data['imgc']):
-                if(i == 0):
-                    continue
-                else:
-                    try:
-                        image = sp.imresize(imread(loc), size=shape)
-                        X_train.append(image[30:96, :])
-                        y_train.append(data['angle'][i])
-                        print("Center camera resizing", i, "/", length - 1)
-                    except OSError:
-                        count += 1
-                        pass
-        elif data[0][i] == 'left':
-            for i, loc in zip(range(length), data['imgl']):
-                if(i == 0):
-                    continue
-                elif(len(loc) == 0):
-                    break
-                else:
-                    try:
-                        image = sp.imresize(imread(loc), size=shape)
-                        X_train.append(image[30:96, :])
-                        y_train.append(data['angle'][i] + shift)
-                        print("Left camera resizing", i, "/", length - 1)
-                    except OSError:
-                        count += 1
-                        pass
-        elif data[0][i] == 'right':
-            for i, loc in zip(range(length), data['imgr']):
-                if(i == 0):
-                    continue
-                elif(len(loc) == 0):
-                    break
-                else:
-                    try:
-                        image = sp.imresize(imread(loc), size=shape)
-                        X_train.append(image[30:96, :])
-                        y_train.append(data['angle'][i] - shift)
-                        print("Right camera resizing", i, "/", length - 1)
-                    except OSError:
-                        count += 1
-                        pass
-    print(count, "files don't exist. Total number of imagaes is ", len(X_train) * 2)
-    print("Fliping..")
-    y_train = np.array(y_train).astype(np.float32)
-    a = []
-    for i in range(len(X_train)):
-        a.append(np.fliplr(X_train[i]))
-    X_train = np.array(X_train)
-    X_train = np.vstack((X_train, a))
-    y_train = np.concatenate((y_train, -y_train))
-    print("X_train shape: ", X_train.shape)
-    print("y_train shape: ", y_train.shape)
-    return X_train, y_train
-
+	length = len(data['imgc'])
+	X_train = []
+	y_train = []
+	count = 0
+	for i in range(3):
+		if data[0][i] == 'center':
+			for i, loc in zip(range(length), data['imgc']):
+				if(i == 0):
+					continue
+				else:
+					try:
+						image = sp.imresize(imread(loc), size=shape)
+						X_train.append(image[30:96, :])
+						y_train.append(data['angle'][i])
+						print("Center camera resizing", i, "/", length - 1)
+					except OSError:
+						count += 1
+						pass
+		elif data[0][i] == 'left':
+			for i, loc in zip(range(length), data['imgl']):
+				if(i == 0):
+					continue
+				elif(len(loc) == 0):
+					break
+				else:
+					try:
+						image = sp.imresize(imread(loc), size=shape)
+						X_train.append(image[30:96, :])
+						y_train.append(data['angle'][i] + shift)
+						print("Left camera resizing", i, "/", length - 1)
+					except OSError:
+						count += 1
+						pass
+		elif data[0][i] == 'right':
+			for i, loc in zip(range(length), data['imgr']):
+				if(i == 0):
+					continue
+				elif(len(loc) == 0):
+					break
+				else:
+					try:
+						image = sp.imresize(imread(loc), size=shape)
+						X_train.append(image[30:96, :])
+						y_train.append(data['angle'][i] - shift)
+						print("Right camera resizing", i, "/", length - 1)
+					except OSError:
+						count += 1
+						pass
+	print(count, "files don't exist. Total number of imagaes is ", len(X_train) * 2)
+	print("Fliping..")
+	y_train = np.array(y_train).astype(np.float32)
+	a = []
+	for i in range(len(X_train)):
+		a.append(np.fliplr(X_train[i]))
+	X_train = np.array(X_train)
+	X_train = np.vstack((X_train, a))
+	y_train = np.concatenate((y_train, -y_train))
+	print("X_train shape: ", X_train.shape)
+	print("y_train shape: ", y_train.shape)
+	return X_train, y_train
 
 def save():
-    #dummy
-    #data = np.genfromtxt('./dummy.csv',dtype=[('imgc','U110'),('imgl','U110'),('imgr','U110'),('angle','f8')],delimiter=",",usecols=(0,1,2,3))
-    #Real
-    data = np.genfromtxt('./data/driving_log.csv',dtype=[('imgc','U110'),('imgl','U110'),('imgr','U110'),('angle','f8')],delimiter=",",usecols=(0,1,2,3))
-    X_train, y_train = flip_merge(data)
-    np.save("X_train", X_train)
-    np.save("y_train", y_train)
-    return X_train, y_train
-
+	#dummy
+	#data = np.genfromtxt('./dummy.csv',dtype=[('imgc','U110'),('imgl','U110'),('imgr','U110'),('angle','f8')],delimiter=",",usecols=(0,1,2,3))
+	#Real
+	#data = np.genfromtxt('./data/driving_log.csv',dtype=[('imgc','U110'),('imgl','U110'),('imgr','U110'),('angle','f8')],delimiter=",",usecols=(0,1,2,3))
+	data = np.genfromtxt('./DATAmine/driving_log.csv',dtype=[('imgc','U110'),('imgl','U110'),('imgr','U110'),('angle','f8')],delimiter=",",usecols=(0,1,2,3))
+	X_train, y_train = flip_merge(data)
+	np.save("X_train", X_train)
+	np.save("y_train", y_train)
+	return X_train, y_train
+	
 
 def load():
-    X_train = np.load("X_train.npy")
-    y_train = np.load("y_train.npy")
-    return X_train, y_train
+	X_train = np.load("X_train.npy")
+	y_train = np.load("y_train.npy")
+	return X_train, y_train
 
 
 def generator(X_train, y_train, batch_size):
-    batch_train = np.zeros((batch_size, 66, 200, 3))
-    batch_angle = np.zeros(batch_size)
-    while True:
-        for i in range(batch_size):
-            batch_train[i] = X_train[i]
-            batch_angle[i] = y_train[i]
-        batch_train / 255 - 0.5  # Nomalization
-        yield batch_train, batch_angle
-
+	batch_train = np.zeros((batch_size, 66, 200, 3))
+	batch_angle = np.zeros(batch_size)
+	while True:
+		for i in range(batch_size):
+			batch_train[i] = X_train[i]
+			batch_angle[i] = y_train[i]
+			batch_train[i] = batch_train[i]/255.-.5  # Nomalization
+		yield batch_train, batch_angle
 
 # Data initialize
 X_train = []
 y_train = []
 if(data_create_or_load == 0):
-    X_train, y_train = save()
+	X_train, y_train = save()
 else:
-    X_train, y_train = load()
+	X_train, y_train = load()
 
 
 # Shuffle and Split into Train and Validate set
@@ -174,10 +171,10 @@ model.compile(Adam(lr=learning_rate), loss='mse', metrics=['accuracy'])
 
 # Train
 model.fit_generator(generator(X_train, y_train, 128),
-                    samples_per_epoch=len(X_train),
-                    nb_epoch=epoch,
-                    validation_data=generator(X_train, y_train, 128),
-                    nb_val_samples=len(X_validate))
+	samples_per_epoch=len(X_train),
+	nb_epoch=epoch,
+	validation_data=generator(X_train, y_train, 128),
+	nb_val_samples=len(X_validate))
 
 # Save Model
 from keras.models import load_model
